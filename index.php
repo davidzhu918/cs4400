6bc0e98a06dbd5ce03d469f4dc80c7ca0c218dab
yxjames <zhugeyx@163.com>
2015-11-22 17:31:17 -0500

    4

--- index.php
+++ index.php
@@ -25,8 +25,8 @@ $_SESSION['pwd'] = $pwd;
 mysql_connect($host,$username,$password) or die( "Unable to connecthaha");;
 mysql_select_db($database) or die( "Unable to select database");
 //Our SQL Query
-$sql_query = "Select Username From CUSTOMER 
-           Where (Username = $usn AND Password = $pwd)";  
+$sql_query = "SELECT Username FROM CUSTOMER 
+           WHERE (Username = $usn AND Password = $pwd)";  
 //Run our sql query
  $result = mysql_query ($sql_query)  or die(mysql_error());  
 
