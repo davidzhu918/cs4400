1f21272647fd1c4b0c2f365de5e930ce5ba819c4
yxjames <zhugeyx@163.com>
2015-11-22 18:51:36 -0500

    know

--- functionality.php
+++ functionality.php
@@ -1,27 +1,42 @@
 <?php
-	session_start();
-	include 'dbinfo.php';
-	$usn = $_SESSION['username'];
-	$identity = $_SESSION['identity'];
+        session_start();
+        include 'dbinfo.php';
+        $usn = $_SESSION['username'];
+        $identity = $_SESSION['identity'];
 ?>
 
 <html>
 <head>
-	<title>Hi</title>
+        <title>Hi</title>
 </head>
 <body>
 <center>
-<br /><br /><br />
+<br><br><br>
 <b><p>Choose functionality</p></b>
 <b><p>Hi <?php echo $usn ?></p></b>
 <?php
+mysql_connect($host,$username,$password) or die( "Unable to connect");;
+mysql_select_db($database) or die( "Unable to select database");
 
 if ($identity == "customer") {
 
-	echo "<form action=\"index.php\" method=\"post\">";
-	echo "Employee Query";
-	echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
-	echo "</form>";
+        echo "<form action=\"index.php\" method=\"post\">";
+        echo "Make a new reservation";
+        echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "<form action=\"index.php\" method=\"post\">";
+        echo "Update your reservation";
+        echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "Cancel Reservation";
+        echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "Provide feedback";
+        echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "View feedback";
+        echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+
+        echo "</form>";
+
 }
 
 ?>
