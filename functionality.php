09829f869859782fd873f27a73e2275072b17a84
yxjames <zhugeyx@163.com>
2015-11-22 19:00:46 -0500

    are

--- functionality.php
+++ functionality.php
@@ -20,6 +20,7 @@ mysql_select_db($database) or die( "Unable to select database");
 
 if ($identity == "customer") {
 
+		echo "<a href=\"index.php\"> Make a new reservation </a>";
         echo "<form action=\"index.php\" method=\"post\">";
         echo "Make a new reservation";
         echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
