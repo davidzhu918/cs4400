083d1c5060c4ad54adb1a9725e26f910b22a29e2
yxjames <zhugeyx@163.com>
2015-11-22 19:10:47 -0500

    frog

--- functionality.php
+++ functionality.php
@@ -19,23 +19,35 @@ mysql_connect($host,$username,$password) or die( "Unable to connect");;
 mysql_select_db($database) or die( "Unable to select database");
 
 if ($identity == "customer") {
+		
 		echo "<a href=\"index.php\"> <p>Make a new reservation</p></a>";
+
         echo "<form action=\"index.php\" method=\"post\">";
+        
         echo "Make a new reservation";
         echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "</form>";
+
         echo "<form action=\"index.php\" method=\"post\">";
         echo "Update your reservation";
         echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "</form>";
+
+        echo "<form action=\"index.php\" method=\"post\">";
         echo "Cancel Reservation";
         echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
-        echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "</form>";
+
+        echo "<form action=\"index.php\" method=\"post\">";
         echo "Provide feedback";
         echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
-        echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+        echo "</form>";
+        
+        echo "<form action=\"index.php\" method=\"post\">";
         echo "View feedback";
         echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
-
         echo "</form>";
+
 
 }
 
