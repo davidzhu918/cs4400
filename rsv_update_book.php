2ccb6ea6e4f1485c5480f591767d69f36e385b05
yvonnesjy <stonfish@hotmail.com>
2015-12-01 02:37:21 -0500

    Disabled add bed checkbox in update

--- rsv_update_book.php
+++ rsv_update_book.php
@@ -78,8 +78,7 @@ Rooms are available. Please confirm details below before submitting.
         echo "<td>$ ".$row['CostPerDay']."</td>";
         echo "<td>$ ".$row['CostBedPerDay']."</td>";
         $checked = $row['IncludeExtraBed'] == 1 ? "checked=\"checked\"" : "checked=\"unchecked\"";
-        echo "<td><input type=\"checkbox\" ".$checked."/></td>";
-        // echo "<td><input type=\"checkbox\" checked=\"".$row['IncludeExtraBed'] == 1 ? "checked" : "unchecked"."/></td>";
+        echo "<td><input type=\"checkbox\" ".$checked." disabled=\"disabled\" /></td>";
         echo "</tr>";
     }
     ?>
